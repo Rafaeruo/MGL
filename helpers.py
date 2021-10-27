613795208aefb1de8f2bbad1f0fd74cab06b3087
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2021-10-27 10:41:52 -0300

    regenerate igdb access token on expiry

--- helpers.py
+++ helpers.py
@@ -6,8 +6,31 @@ from random import randint
 
 import json
 from igdb.wrapper import IGDBWrapper
-wrapper = IGDBWrapper(os.getenv("CLIENT_ID"), os.getenv("AUTHORIZATION"))
 
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+authorization = os.getenv("AUTHORIZATION")
+
+global wrapper
+wrapper = IGDBWrapper(client_id, authorization)
+
+def api_request(params):
+    global wrapper
+    try:
+        return wrapper.api_request(*params)
+    except requests.HTTPError:
+        regen_auth()
+        wrapper = IGDBWrapper(client_id, os.getenv("AUTHORIZATION"))
+        return wrapper.api_request(*params)
+
+def regen_auth():
+    response = requests.post(f"https://id.twitch.tv/oauth2/token?grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}")
+    
+    if (response.status_code == 200):
+        data = response.json()
+        os.environ["AUTHORIZATION"] = data["access_token"]
+    
+  
 #https://flask.palletsprojects.com/en/1.0.x/patterns/viewdecorators/
 def login_required(f):
     @wraps(f)
@@ -20,37 +43,37 @@ def login_required(f):
 def api_game_id_lookup(game_ids):#returns json of game or None
     if not game_ids:
         return
-    byte_array = wrapper.api_request(
+    byte_array = api_request([
             'games',
             f'fields id, name, slug, cover.url; where id = ({",".join(map(str, game_ids))}); limit 500;'
-            )
+    ])
     return json.loads(byte_array)
 
 def api_game_slug_lookup(game_slug):#returns json of game or None
     if not game_slug:
         return
-    byte_array = wrapper.api_request(
+    byte_array = api_request([
             'games',
             f'fields id, name, summary, slug, cover.url, platforms.name, genres.name, first_release_date, involved_companies.company.name, involved_companies.developer, involved_companies.publisher; where slug = "{game_slug}";'
-            )
+    ])
     return json.loads(byte_array)
 
 def api_game_name_lookup(game_name, page, limit, limit_display):#returns json of games or None
     if not game_name:
         return
     page = (int(page)-1)*limit_display
-    byte_array = wrapper.api_request(
+    byte_array = api_request([
             'games',
             f'fields name, slug, cover.url; search "{game_name}"; limit ${limit}; offset ${page};'
-            )
+    ])
     return json.loads(byte_array)
 
 def api_get_random(n):
     ids = []
     for i in range(n):
         ids.append(randint(0, 142043))#some ids don't actually exist but it doesn't matter
-    byte_array = wrapper.api_request(
+    byte_array = api_request([
             'games',
             f'fields id, name, cover.url, slug; where id = ({",".join(map(str, ids))}); limit {n};' 
-            )
+    ])
     return json.loads(byte_array)
